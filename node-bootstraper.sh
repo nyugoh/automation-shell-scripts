30f555e642f147f75a918819fa1efef1821e92d4
nyugoh <nyugoh@gmail.com>
2019-04-25 09:09:35 +0000

    Added yarn installation steps :rocket: :+1:

--- node-bootstraper.sh
+++ node-bootstraper.sh
@@ -11,9 +11,11 @@ sudo apt install curl -y
 cd /temp
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+echo "Installing node 8.11.0"
 
 nvm install 8.11.0
 
@@ -21,9 +23,23 @@ nvm use 8.11.0
 
 echo "Checking node version"
 
-echo node -v
+node -v
 
+echo "Installing Yarn"
 
+echo
+
+echo
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install --no-install-recommends yarn
+
+yarn -v
+
+echo "Bye,"
 
 
 
